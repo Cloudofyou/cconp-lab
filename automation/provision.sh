a95036eddeadc3273e1cf98850ab50045db36f6e
Glenn Wiseman <gwiseman@cumulusnetworks.com>
2019-03-15 13:48:43 -0500

    updated provision script

--- automation/provision.sh
+++ automation/provision.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 ansible-playbook cconp-lab.yaml
+ansible server01 -a "sudo apt-get update -y"
+ansible server01 -a "sudo apt install traceroute -y"
 ansible server01 -a "sudo ip link set up eth1"
 ansible server01 -a "sudo ip addr add 192.168.100.1/24 dev eth1"
 ansible server01 -a "sudo ip route del default"
 ansible server01 -a "sudo ip route add default via 192.168.100.254"
+ansible server02 -a "sudo apt-get update -y"
+ansible server02 -a "sudo apt install traceroute -y"
 ansible server02 -a "sudo ip link set up eth2"
 ansible server02 -a "sudo ip addr add 192.168.100.2/24 dev eth2"
 ansible server02 -a "sudo ip route del default"
