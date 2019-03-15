e5c712335ad0445d14d50f17545f9fabe514d056
Glenn Wiseman <gwiseman@cumulusnetworks.com>
2019-03-15 12:31:07 -0500

    updated provision.sh to update servers

--- automation/provision.sh
+++ automation/provision.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 ansible-playbook cconp-lab.yaml
+ansible server01 -a "sudo ip link set up eth1"
+ansible server01 -a "sudo ip addr add 192.168.100.1/24 dev eth1"
+ansible server02 -a "sudo ip link set up eth2"
+ansible server02 -a "sudo ip addr add 192.168.100.2/24 dev eth2"
+
+
