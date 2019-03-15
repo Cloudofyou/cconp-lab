49352330551e18f7ec55691d6d86682eaeb2c54c
Glenn Wiseman <gwiseman@cumulusnetworks.com>
2019-03-15 13:25:23 -0500

    fixed automation script

--- automation/provision.sh
+++ automation/provision.sh
@@ -2,7 +2,10 @@
 ansible-playbook cconp-lab.yaml
 ansible server01 -a "sudo ip link set up eth1"
 ansible server01 -a "sudo ip addr add 192.168.100.1/24 dev eth1"
+ansible server01 -a "sudo ip route del default"
+ansible server01 -a "sudo ip route add default via 192.168.100.254"
 ansible server02 -a "sudo ip link set up eth2"
 ansible server02 -a "sudo ip addr add 192.168.100.2/24 dev eth2"
-
+ansible server02 -a "sudo ip route del default"
+ansible server02 -a "sudo ip route add default via 192.168.100.254"
 
